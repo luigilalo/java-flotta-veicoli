e96c7ec74c00c2bacabb52e4d59362a90f72b92e
luigilalo <luigilaloe1@gmail.com>
2023-12-14 17:24:09 +0100

    Method to find Vehicle to improve

--- src/flotta/veicoli/org/GestoreFlotta.java
+++ src/flotta/veicoli/org/GestoreFlotta.java
@@ -20,4 +20,5 @@ public class GestoreFlotta {
         }
         return contatore;
     }
+
 }
--- src/flotta/veicoli/org/Main.java
+++ src/flotta/veicoli/org/Main.java
@@ -2,6 +2,12 @@ package flotta.veicoli.org;
 
 public class Main {
     public static void main(String[] args) {
-
+        GestoreFlotta gestore = new GestoreFlotta();
+        Automobili auto1 = new Automobili("123ABC", 2002, 5);
+        gestore.addVeicolo(auto1);
+        Motociclette moto1 = new Motociclette("456FGH", 1990, true);
+        gestore.addVeicolo(moto1);
+        System.out.println(gestore.contaVeicoli(Automobili.class));
+        System.out.println(gestore.contaVeicoli(Motociclette.class));
     }
 }
